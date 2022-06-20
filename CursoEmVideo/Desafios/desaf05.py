d5a9f98231ff8bb166db368013adc0f002100099
Alexis Cervantes <iacervantes@outlook.com>
2022-06-19 21:57:15 -0300

    Australia fue mas?

---
+++ CursoEmVideo/Desafios/desaf05.py
@@ -0,0 +1,5 @@
+print('==== Desafio 5 ===')
+# Crie um programa que leia um numero inteiro e mostre  na tela seu sucessor e seu antecessor.
+
+
+
---
+++ CursoEmVideo/Desafios/desaf06.py
@@ -0,0 +1,5 @@
+print('==== Desafio 6 ===')
+# Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e sua raiz quadrada.
+
+
+
