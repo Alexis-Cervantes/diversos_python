ef7447a8d53fa1265fd449eaf5fea7f93ce8c737
Alexis Cervantes <iacervantes@outlook.com>
2023-01-13 12:42:25 -0300

    Interactividad com codigo clonado
    vamos a ver si pemite cambios desde la misma cuenta de vscode pero en otra maquina....ESTOY EMOCIONADO

--- adote/venv/Scripts/activate.bat
+++ adote/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Iacer\Workstation\Programar\diversos_python\adote\venv
+set VIRTUAL_ENV=D:\programando\diversos_python\adote\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
